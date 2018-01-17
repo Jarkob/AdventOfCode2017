9bedb0f35e3dd61951f29a874b15485a87e2291b
Jakob Bussas <jakobbu@gmx.de>
2018-01-17 22:18:03 +0100

    Day20

---
+++ Day20/Day20/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Day20
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] Particles = File.ReadAllLines("../../Day20.txt");
+        }
+    }
+}
