703c034319ac2f793a583780d5b0aefecbf09f2a
Jakob Bussas <jakobbu@gmx.de>
2018-01-27 00:38:28 +0100

    Day22 Part1

--- Day22/Day22/Program.cs
+++ Day22/Day22/Program.cs
@@ -9,56 +9,56 @@ namespace Day22
         {
             string[] Input = File.ReadAllLines("../../Day22.txt");
 
-            Input = null;
+            //Input = null;
 
-            Input = new string[]
-            {
-            ".........",
-            ".........",
-            ".........",
-            ".....#...",
-            "...#.....",
-            ".........",
-            ".........",
-            "........."
-            };
+            //Input = new string[]
+            //{
+            //".........",
+            //".........",
+            //".........",
+            //".....#...",
+            //"...#.....",
+            //".........",
+            //".........",
+            //"........."
+            //};
 
             // Try to increase Grid by factor 3
 
-            string[] NewInput = new string[Input.Length * 3];
+            string[] NewInput = new string[Input.Length * 303];
 
             // erst leere Reihen
-            for (int i = 0; i < Input.Length; i++)
+            for (int i = 0; i < Input.Length * 151; i++)
             {
                 NewInput[i] = "";
-                for (int j = 0; j < Input.Length * 3; j++)
+                for (int j = 0; j < Input.Length * 303; j++)
                 {
                     NewInput[i] += ".";
                 }
             }
 
             // einfügen
-            for (int i = Input.Length; i < Input.Length * 2; i++)
+            for (int i = Input.Length * 151; i < Input.Length * 152; i++)
             {
                 NewInput[i] = "";
-                for (int j = 0; j < Input.Length; j++)
+                for (int j = 0; j < Input.Length * 151; j++)
                 {
                     NewInput[i] += ".";
                 }
 
-                NewInput[i] += Input[i - Input.Length];
+                NewInput[i] += Input[i - (Input.Length * 151)];
 
-                for (int k = 0; k < Input.Length; k++)
+                for (int k = 0; k < Input.Length * 151; k++)
                 {
                     NewInput[i] += ".";
                 }
             }
 
             // mehr leere Reihen
-            for (int i = Input.Length * 2; i < Input.Length * 3; i++)
+            for (int i = Input.Length * 152; i < Input.Length * 303; i++)
             {
                 NewInput[i] = "";
-                for (int j = 0; j < Input.Length * 3; j++)
+                for (int j = 0; j < Input.Length * 303; j++)
                 {
                     NewInput[i] += ".";
                 }
@@ -72,16 +72,17 @@ namespace Day22
                 Grid[i] = NewInput[i].ToCharArray();
             }
 
-            Virus Test = new Virus(Grid, 4 + Input.Length, 4 + Input.Length);
+            Virus Test = new Virus(Grid, 12 + (Input.Length * 151), 12 + (Input.Length * 151));
 
             for (int j = 0; j < 10000; j++)
             {
                 Test.Burst();
             }
 
-            Test.Print();
+            //Test.Print();
 
             Console.Write("Amount of Infections: " + Test.Infections);
+            // 5460
         }
     }
 }
