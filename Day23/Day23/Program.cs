74303b14435dc1b45ccf6d118566def17168030c
Jakob Bussas <jakobbu@gmx.de>
2018-01-30 19:06:57 +0100

    Day23 Part1

--- Day23/Day23/Program.cs
+++ Day23/Day23/Program.cs
@@ -31,6 +31,77 @@ namespace Day23
             Registers.Add('f', 0);
             Registers.Add('g', 0);
             Registers.Add('h', 0);
+
+            int MulInvokes = 0;
+
+            for (int i = 0; i < Commands.Length; i++)
+            {
+                switch (Commands[i][0])
+                {
+                    case "set":
+                        if (int.TryParse(Commands[i][2], out var Trash00))
+                        {
+                            Registers[Commands[i][1][0]] = Convert.ToInt32(Commands[i][2]);
+                        }
+                        else
+                        {
+                            Registers[Commands[i][1][0]] = Registers[Commands[i][2][0]];
+                        }
+                        break;
+                    case "sub":
+                        if (int.TryParse(Commands[i][2], out var Trash01))
+                        {
+                            Registers[Commands[i][1][0]] -= Convert.ToInt32(Commands[i][2]);
+                        }
+                        else
+                        {
+                            Registers[Commands[i][1][0]] -= Registers[Commands[i][2][0]];
+                        }
+                        break;
+                    case "mul":
+                        if (int.TryParse(Commands[i][2], out var Trash02))
+                        {
+                            Registers[Commands[i][1][0]] *= Convert.ToInt32(Commands[i][2]);
+                        }
+                        else
+                        {
+                            Registers[Commands[i][1][0]] *= Registers[Commands[i][2][0]];
+                        }
+
+                        MulInvokes++;
+                        break;
+                    case "jnz":
+                        // TODO möglicherweise funktionieren diese breaks nicht richtig
+                        if (int.TryParse(Commands[i][1], out var Trash03))
+                        {
+                            if (Convert.ToInt32(Commands[i][1]) == 0)
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (Registers[Commands[i][1][0]] == 0)
+                            {
+                                break;
+                            }
+                        }
+
+                        if(int.TryParse(Commands[i][2], out var Trash04))
+                        {
+                            i += Convert.ToInt32(Commands[i][2]);
+                        } else {
+                            i += (int)Registers[Commands[i][2][0]]; // TODO mögliches Problem
+                        }
+                        i--;
+                        break;
+                    default:
+                        throw new Exception("Unknown command: " + Commands[i][0]);
+                }
+            }
+
+            Console.WriteLine("Amount of mul: "+ MulInvokes);
+            // 3969
         }
     }
 }
