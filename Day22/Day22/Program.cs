4c9e34327d453520538c4f4c0855001780e70fbc
Jakob Bussas <jakobbu@gmx.de>
2018-01-30 15:45:12 +0100

    Day22 completed

--- Day22/Day22/Program.cs
+++ Day22/Day22/Program.cs
@@ -8,17 +8,17 @@ namespace Day22
     {
         public static void Main()
         {
-            Version2();
+            Part1();
+            Part2();
         }
 
 
-        public static void Version1()
+        public static void Part1()
         {
             string[] Input = File.ReadAllLines("../../Day22.txt");
 
             // Test
             //Input = null;
-
             //Input = new string[]
             //{
             //".........",
@@ -30,90 +30,6 @@ namespace Day22
             //".........",
             //"........."
             //};
-
-            // Es macht Sinn die Grenzen zuerst zu berechnen
-            int Half = 301 * Input.Length;
-            int End = ((Half * 2) + 1) * Input.Length;
-            int HalfEnd = (301 + 1) * Input.Length;
-
-            char[][] NewInput = new char[End][];
-
-            for (int i = Half; i < HalfEnd; i++)
-            {
-                NewInput[i] = new char[End];
-
-                for (int l = Half; l < HalfEnd; l++)
-                {
-                    NewInput[i][l] = Input[i - Half][l - Half];
-                }
-            }
-
-            // Test
-            //NewInput = null;
-            //NewInput = new char[Input.Length][];
-            //for (int i = 0; i < Input.Length; i++) {
-            //    NewInput[i] = Input[i].ToCharArray();
-            //}
-
-            Virus Test = new Virus(NewInput, 12 + Half, 12 + Half);
-            // Test
-            //Test = null;
-            //Test = new Virus(NewInput, 4 + Half, 4 + Half);
-
-            // measure
-            int[] Values0 = new int[1000];
-            int[] Values = new int[1000];
-
-
-            for (int j = 0; j < 24000; j++)
-            {
-                if (j >= 22000 && j < 23000)
-                {
-                    Values0[j - 22000] = Test.Infections;
-                }
-                if (j >= 23000)
-                {
-                    Values[j - 23000] = Test.Infections;
-                }
-                Console.WriteLine("Infections after " + j + ": " + Test.Infections);
-                //Test.Burst1(); // Part1
-                Test.Burst2();
-            }
-
-            //Test.Print();
-            int Average = Values0[999] - Values0[0];
-            Console.WriteLine("change from 22000 to 23000 = " + (Values0[999] - Values0[0]));
-            Console.WriteLine("change from 23000 to 24000 = " + (Values[999] - Values[0]));
-
-            // 9976000
-            int Result = Test.Infections + (9976000 / 1000 * Average);
-
-            Console.Write("Amount of Infections: " + Result);
-            // 5460
-            // 3204753 too high
-            // 3454 too low
-            // 1420046 too low
-        }
-
-
-        public static void Version2()
-        {
-            string[] Input = File.ReadAllLines("../../Day22.txt");
-
-            // Test
-            Input = null;
-
-            Input = new string[]
-            {
-            ".........",
-            ".........",
-            ".........",
-            ".....#...",
-            "...#.....",
-            ".........",
-            ".........",
-            "........."
-            };
 
             IDictionary<(int, int), char> Grid = new Dictionary<(int, int), char>();
 
@@ -132,11 +48,7 @@ namespace Day22
 
             int Infections = 0;
 
-            // Test
-            X += 100;
-            Y += 100;
-
-            for (int i = 0; i < 70; i++)
+            for (int i = 0; i < 10000; i++)
             {
                 if (!Grid.ContainsKey((X, Y)))
                 {
@@ -167,7 +79,7 @@ namespace Day22
                         Grid[(X, Y)] = '.';
                         break;
                     case '.':
-                        switch(Direction)
+                        switch (Direction)
                         {
                             case "up":
                                 Direction = "left";
@@ -194,7 +106,7 @@ namespace Day22
                 }
 
                 // Moven
-                switch(Direction)
+                switch (Direction)
                 {
                     case "up":
                         Y--;
@@ -213,7 +125,149 @@ namespace Day22
                 }
             }
 
-            Console.WriteLine("Anzahl Infektionen: "+ Infections);
+            Console.WriteLine("Anzahl Infektionen: " + Infections);
+            // 5460
+        }
+
+
+        public static void Part2()
+        {
+            string[] Input = File.ReadAllLines("../../Day22.txt");
+
+            // Test
+            //Input = null;
+            //Input = new string[]
+            //{
+            //".........",
+            //".........",
+            //".........",
+            //".....#...",
+            //"...#.....",
+            //".........",
+            //".........",
+            //"........."
+            //};
+
+            IDictionary<(int, int), char> Grid = new Dictionary<(int, int), char>();
+
+            for (int i = 0; i < Input.Length; i++)
+            {
+                for (int j = 0; j < Input[i].Length; j++)
+                {
+                    Grid.Add((j, i), Input[i][j]);
+                }
+            }
+
+            // Jetzt moven
+            int X = Input[0].Length / 2;
+            int Y = Input.Length / 2;
+            string Direction = "up";
+
+            int Infections = 0;
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                if (!Grid.ContainsKey((X, Y)))
+                {
+                    Grid.Add((X, Y), '.');
+                }
+
+                switch (Grid[(X, Y)])
+                {
+                    case '#':
+                        switch (Direction)
+                        {
+                            case "up":
+                                Direction = "right";
+                                break;
+                            case "down":
+                                Direction = "left";
+                                break;
+                            case "left":
+                                Direction = "up";
+                                break;
+                            case "right":
+                                Direction = "down";
+                                break;
+                            default:
+                                throw new Exception("Unknown direction");
+                        }
+
+                        Grid[(X, Y)] = 'F';
+                        break;
+                    case '.':
+                        switch (Direction)
+                        {
+                            case "up":
+                                Direction = "left";
+                                break;
+                            case "down":
+                                Direction = "right";
+                                break;
+                            case "left":
+                                Direction = "down";
+                                break;
+                            case "right":
+                                Direction = "up";
+                                break;
+                            default:
+                                throw new Exception("Unknown direction");
+                        }
+
+                        Grid[(X, Y)] = 'W';
+                        break;
+                    case 'W':
+                        Grid[(X, Y)] = '#';
+                        Infections++;
+                        break;
+                    case 'F':
+                        // Reverse direction
+                        switch(Direction)
+                        {
+                            case "up":
+                                Direction = "down";
+                                break;
+                            case "down":
+                                Direction = "up";
+                                break;
+                            case "left":
+                                Direction = "right";
+                                break;
+                            case "right":
+                                Direction = "left";
+                                break;
+                            default:
+                                throw new Exception("Unknown direction");
+                        }
+
+                        Grid[(X, Y)] = '.';
+                        break;
+                    default:
+                        throw new Exception("Unknown state");
+                }
+
+                // Moven
+                switch (Direction)
+                {
+                    case "up":
+                        Y--;
+                        break;
+                    case "down":
+                        Y++;
+                        break;
+                    case "left":
+                        X--;
+                        break;
+                    case "right":
+                        X++;
+                        break;
+                    default:
+                        throw new Exception("Unknown direction");
+                }
+            }
+
+            Console.WriteLine("Anzahl Infektionen: " + Infections);
+            // 2511702
         }
 
 
@@ -221,7 +275,45 @@ namespace Day22
         {
             // Von oben links nach unten rechts
             // Erst Maximalwerte herausfinden
-            // Keine Ahnung wie man das darstellen soll, TODO
+            int MaxX = 0;
+            int MaxY = 0;
+            int MinX = 0;
+            int MinY = 0;
+
+            foreach (var element in Grid)
+            {
+                if (element.Key.Item1 > MaxX)
+                {
+                    MaxX = element.Key.Item1;
+                }
+                if (element.Key.Item1 < MinX)
+                {
+                    MinX = element.Key.Item1;
+                }
+                if (element.Key.Item2 > MaxY)
+                {
+                    MaxY = element.Key.Item2;
+                }
+                if (element.Key.Item2 < MinY)
+                {
+                    MinY = element.Key.Item2;
+                }
+            }
+
+
+            // Jetzt anzeigen
+            for (int i = MinY; i <= MaxY; i++)
+            {
+                for (int j = MinX; j <= MaxX; j++)
+                {
+                    if (!Grid.ContainsKey((j, i)))
+                    {
+                        Grid.Add((j, i), '.');
+                    }
+                    Console.Write(Grid[(j, i)]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
