d44f801de5dbc43f5b7e16cf31e3b4481a9609c9
Jakob Bussas <jakobbu@gmx.de>
2018-02-08 21:24:39 +0100

    Day23 continue

--- Day23/Day23/Program.cs
+++ Day23/Day23/Program.cs
@@ -137,6 +137,58 @@ namespace Day23
             // Doesn't work
 
             Console.WriteLine("Value of h: " + Registers['h']);
+
+            // Try to refactor
+            int a = 1;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int e = 0;
+            int f = 0;
+            int g = 0;
+            int h = 0;
+
+            b = 65;
+            c = b;
+            if (a != 0) { goto A; }
+            goto B;
+            b *= 100;
+        A:
+            b -= 100000;
+            c = b;
+            c -= 17000;
+        B:
+            f = 1;
+            d = 2;
+        E:
+            e = 2;
+        D:
+            g = d;
+            g *= e;
+            g -= b;
+            if (g != 0) { goto C; }
+            f = 0;
+        C:
+            e -= 1;
+            g = e;
+            g -= b;
+            if (g != 0) { goto D; }
+            d -= 1;
+            g = d;
+            g -= b;
+            if (g != 0) { goto E; }
+            if (f != 0) { goto F; }
+            h -= 1;
+        F:
+            g = b;
+            g -= c;
+            if (g != 0) { goto G; }
+            goto H;
+        G:
+            b -= 17;
+            goto B;
+        H:
+            Console.WriteLine(h);
         }
     }
 }
